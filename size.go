24bf7f51ae3f05850339f2afe53b08348b1ffbd6
Patrick Dahlke <patrick.dahlke@pichu.io>
2014-12-05 14:09:24 +0100

    base type implementation for directory tree visit
    next will follow soon

---
+++ size.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"strconv"
+)
+
+type Size int64
+
+var suffix = [...]string{"Bytes", "KBytes", "MBytes", "GBytes", "TBytes"}
+
+func (s Size) String() string {
+	i := 0
+	for int64(s) > 50000 && i < len(suffix)-1 {
+		s = s / 1000
+		i++
+	}
+	return strconv.FormatInt(int64(s), +" "+suffix[i])
+}
